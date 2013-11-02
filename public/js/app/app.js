450bd9f215891325ddf2e03f5ef8fb67528a3195
Chyld Medford <chyld.medford@gmail.com>
2013-11-02 14:57:44 -0500

    final demo code

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -2,4 +2,21 @@ $(document).ready(initialize);
 
 function initialize(){
   $(document).foundation();
+  onClickHandlers();
+}
+
+function onClickHandlers(){
+  $('.digit').on('click', clickDigit);
+}
+
+function offClickHandlers(){
+  $('.digit').off('click');
+}
+
+function clickDigit(){
+  var display = $('#display').text();
+  var number = $(this).text();
+  var output = display + number;
+  output = parseInt(output, 10);
+  $('#display').text(output);
 }
--- public/js/tests/master.js
+++ public/js/tests/master.js
@@ -1,7 +1,20 @@
 module('Integration Testing', {setup: setupTest, teardown: teardownTest});
 
 function setupTest(){
+  offClickHandlers();
+  onClickHandlers();
 }
 
 function teardownTest(){
 }
+
+test('Put 8 in display', function(){
+  $('#d8').trigger('click');
+  equal($('#display').text(), 8, '8 should be in display');
+});
+
+test('Put 78 in display', function(){
+  $('#d7').trigger('click');
+  $('#d8').trigger('click');
+  equal($('#display').text(), 78, '78 should be in display');
+});
